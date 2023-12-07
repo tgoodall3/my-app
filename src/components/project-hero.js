eefc5b48cb327036fbbc39b4669ffd1f14f7c7c0
tgoodall3 <105135371+tgoodall3@users.noreply.github.com>
2023-12-07 09:04:08 -0500

    project update

--- src/components/project-hero.js
+++ src/components/project-hero.js
@@ -7,9 +7,9 @@ function ProjectHero() {
     <div className='ProjectHero'>
         <h1 className='Head'>Dig into my work</h1>
         <div className="projects" data-aos="fade-right">
-        <Card className="one" text="Description for Project 1"/>
-  <Card className="two" text="Description for Project 2"/>
-  <Card className="three"text="Description for Project 3"/>
+        <Card className="one" text="A React website using the TDMB API to create a great, user-friendly experience."/>
+  <Card className="two" text="This is a front-end design of a Ecommerce brand called Empire Fitness. It is hosted with github Pages."/>
+  <Card className="three"text="This is a React/Firebase creation, a CRUD Todo List. It has some great functionalities and just showcases some of the techniques I have used."/>
   </div>
     </div>
   );
