409693bce15b4ceb3488f54ce6aa882ab062ea38
tgoodall3 <105135371+tgoodall3@users.noreply.github.com>
2023-12-07 20:55:19 -0500

    big changes

--- src/components/project-hero.js
+++ src/components/project-hero.js
@@ -4,16 +4,20 @@ import React from 'react';
 import MVN from '../assets/images/MVN.png';
 import empire from '../assets/images/empire.png';
 import crud from '../assets/images/crud.png';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-function ProjectHero() {
+
+function ProjectHero() {  
   return (
     <div className='ProjectHero'>
         <h1 className='Head'>Dig into my work</h1>
         <div className="projects">
-        <Card className="one" text="A React website using the TDMB API to create a great, user-friendly experience." image={MVN} />
-  <Card className="two" text="A front-end design of a Ecommerce brand called Empire Fitness." image={empire}/>
-  <Card className="three"text="A React/Firebase CRUD Todo List." image={crud}/>
-  </div>
+         <a href='https://movienite.vercel.app/'><Card on className="one" text="A React website using the TDMB API to create a great, user-friendly experience." image={MVN} /></a>
+         <a href='https://tgoodall3.github.io/career-planning---ecom-portfolio/'><Card className="two" text="A front-end design of a Ecommerce brand called Empire Fitness." image={empire}/></a>
+         <a href='https://final-todo-nu.vercel.app/'><Card className="three"text="A   React/Firebase CRUD Todo List." image={crud}/></a>
+      </div>
     </div>
   );
 }
