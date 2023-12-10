3187ec0dfd6bfdfe20e7663f23308a0ac5b9d59f
tgoodall3 <105135371+tgoodall3@users.noreply.github.com>
2023-12-10 13:29:01 -0500

    possbile link image change

--- src/components/project-hero.js
+++ src/components/project-hero.js
@@ -4,6 +4,7 @@ import React from 'react';
 import MVN from '../assets/images/MVN.png';
 import empire from '../assets/images/empire.png';
 import crud from '../assets/images/crud.png';
+import Precision from '../assets/images/precision.png';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
@@ -14,6 +15,7 @@ function ProjectHero() {
     <div className='ProjectHero'>
         <h1 className='Head'>Dig into my work</h1>
         <div className="projects">
+        <a href='https://precision-paint-tgoodall3.vercel.app/'><Card className="four" text="My Latest UNFINISHED work for a local contractor." image={Precision}/></a>
          <a href='https://movienite.vercel.app/'><Card on className="one" text="A React website using the TDMB Movie API." image={MVN} /></a>
          <a href='https://tgoodall3.github.io/career-planning---ecom-portfolio/'><Card className="two" text="A front-end design of a Ecommerce brand called Empire Fitness." image={empire}/></a>
          <a href='https://final-todo-nu.vercel.app/'><Card className="three"text="A   React/Firebase CRUD Todo List." image={crud}/></a>
